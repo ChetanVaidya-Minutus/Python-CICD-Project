01165453a7b99c25e42094d44eeeab74e03c1e73
ChetanVaidya-Minutus <102664173+ChetanVaidya-Minutus@users.noreply.github.com>
2023-03-23 18:13:48 +0530

    Removed secret credentials from app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,6 @@
 provider "aws" {
    region = var.region
-   #shared_credentials_files = ["C:/Users/admin/.aws/credentials"]
-   access_key = "AKIAT6OYGGM6Q2RGCKNG"
-   secret_key = "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"
-   token = "kX9QNgkQZQZN9w.atlasv1.ryZpapzyEj1niFb9W5Vd3WYN3jPqzlYhqfpCSjjRmyQPWYvk5Sbth4ckIvX3BGByLms"
+   
 }
 
 
